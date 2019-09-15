59e6434127c3e65092e9418501796b357dc7d576
Plouf <plouf@pdty.be>
2019-09-16 00:50:11 +0800

    useful script

---
+++ kafka/runall.cmd
@@ -0,0 +1,7 @@
+start "Checker" java -jar checker\build\libs\checker-0.0.1-SNAPSHOT.jar
+start "AML" java -jar aml\build\libs\aml-0.0.1-SNAPSHOT.jar
+start "Processor" java -jar processor\build\libs\processor-0.0.1-SNAPSHOT.jar
+start "Mailer" java -jar mailer\build\libs\mailer-0.0.1-SNAPSHOT.jar
+start "Auditor" java -jar auditor\build\libs\auditor-0.0.1-SNAPSHOT.jar
+start "Accountant" java -jar accountant\build\libs\accountant-0.0.1-SNAPSHOT.jar
+start "Reporter" java -jar reporter\build\libs\reporter-0.0.1-SNAPSHOT.jar
